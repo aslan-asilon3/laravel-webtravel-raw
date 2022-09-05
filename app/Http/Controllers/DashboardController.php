34510940767a43289d4525aeb5d02eab5c254658
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2022-09-05 08:26:40 -0700

    add auth ui

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        return view('pages.admin.index');
+        return view('layouts.app-admin');
     }
 }
